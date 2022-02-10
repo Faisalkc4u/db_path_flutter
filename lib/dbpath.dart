6a31f948023c407ed8c73b768dcfd26701b027d3
faisy <Faisalkc4u@gmail.com>
2022-02-10 20:37:50 +0400

    Update dbpath.dart

--- lib/dbpath.dart
+++ lib/dbpath.dart
@@ -4,7 +4,7 @@ import 'package:flutter/services.dart';
 
 const MethodChannel _channel = const MethodChannel('dbpath');
 
-Future<String?> getDataBasePath() async {
-  final String? path = await _channel.invokeMethod('getDataBasePath');
+Future<String> getDataBasePath() async {
+  final String path = await _channel.invokeMethod('getDataBasePath');
   return path;
 }
